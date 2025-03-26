f45a3f874d4a1d990f481e067dea61f6763e793f
byiringiroaimefils <aimefils173@gmail.com>
2025-03-26 23:35:07 +0200

    Add initial project structure and integrate Tailwind CSS

---
+++ Client/src/App.jsx
@@ -0,0 +1,15 @@
+import react from 'react'
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+
+    <h1>Hello world!</h1>
+    </div>
+  )
+}
+
+export default App
