5ae7fe51c63fa02fd7a7f7b5235fbae4c2f04dc5
byiringiroaimefils <aimefils173@gmail.com>
2025-03-31 22:09:30 +0200

    Testing backend

--- Server/app.js
+++ Server/app.js
@@ -1,6 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const app = express();
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./DB.js');
+const annoucement_router = require('./routers/annoucement.js');
+const event_news_router = require('./routers/event_news.js');
+const PORT = process.env.PORT || 5000;
+dotenv.config();
+connectDB();
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// API Routes
+app.use('/annoucement', annoucement_router);
+app.use('/event_news', event_news_router);
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
