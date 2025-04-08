c226fa1f11940ce51fa9006c2ea4dddd243c811f
loxxcode <loxxhypolite@gmail.com>
2025-04-08 13:41:45 +0200

    admission look changes

--- Client/src/pages/Campus.tsx
+++ Client/src/pages/Campus.tsx
@@ -20,7 +20,7 @@ const Campus = () => {
         <div className="container px-80 justify-center">
           <AnimatedSection animation="slide-up" className="max-w-3xl">
             <h1 className="text-4xl text-center md:text-5xl lg:text-6xl font-display font-bold leading-tight mb-6">
-              Administractive
+              Administrative
             </h1>
             <p className="text-lg text-center md:text-xl text-muted-foreground">
               Experience our vibrant college community and world-class facilities designed to support your academic journey.
@@ -33,13 +33,13 @@ const Campus = () => {
       <div className="py-16 md:py-24 bg-white dark:bg-black">
         <div className="container px-4">
           <AnimatedSection animation="fade-in" className="text-center">
-            <div className='lg:ml-20 flex flex-col lg:flex-row gap-5' >
-            <div className='w-full lg:w-[30%] shadow-lg rounded-md p-2 border-2 border-gray'>
+            <div className='lg:ml-20 w-[1100px]'>
+            <div className='w-full flex gap-5 shadow-lg rounded-md p-2 border-b-2 border-gray'>
 
-              <img src={Running} alt="activity" className='w-full p-2 h-[300px]'/>
-              <h2>Dr. John Smith</h2>
-              <div>
-                <p className='text-start justify-start'>At the helm of ESTG is our esteemed principal, Dr. John Smith, a visionary leader with a deep passion for education and student development.....</p> 
+              <img src={Running} alt="activity" className='w-[50%] p-2 h-[300px] rounded-md'/>
+              <div className='p-2'>
+                <h1 className='p-4'>Dr. John Smith 1</h1>
+                <p className='text-start justify-start'>At the helm of ESTG is our esteemed principal, Dr. John Smith, a visionary leader with a deep passion for education and student development.</p> 
                  <div className="p-1">
                    
 
@@ -47,27 +47,29 @@ const Campus = () => {
                     <input type="checkbox" id="toggle" className="peer hidden" />
 
                     {/* Content that appears when checkbox is checked */}
-                    <div className="hidden peer-checked:block">
-                      <p className='text-start mb-4'> With over 20 years of experience in the field, Dr. Smith has dedicated his career to fostering environments where students can excel academically, socially, and emotionally.</p>
-                      <label htmlFor="toggle" className="cursor-pointer mt-5 ml-28 flex w-28 bg-blue-500 text-white p-2 rounded">
-                        See Less
-                      </label>
+                    <div className="hidden peer-checked:block flex">
+                      <p className='text-start'> With over 20 years of experience in the field, Dr. Smith has dedicated his career to fostering environments where students can excel academically, socially, and emotionally. 
+                        <label htmlFor="toggle" className="cursor-pointer w-28 pl-2 text-blue-500 rounded">
+                          See Less
+                        </label>
+                      </p>
                     </div>
 
                      {/* Label acts as a clickable button */}
-                    <label htmlFor="toggle" className="cursor-pointer mt-5 ml-28 flex w-28 peer-checked:hidden bg-blue-500 text-white p-2 rounded">
+                    <label htmlFor="toggle" className="cursor-pointer w-28 peer-checked:hidden text-blue-500 p-2 rounded">
+                      <p className='absolute mt-[-28px] ml-[473px] cursor-default'>.....</p>
                       See More
                     </label>
                   </div>
               </div>
             </div>
 
-            <div className='w-full lg:w-[30%] shadow-lg border-opacity-5 rounded-md p-2 border-2 border-gray'>
+            <div className='w-full flex gap-5 shadow-lg mt-4 border-opacity-5 rounded-md p-2 border-b-2 border-gray'>
 
-              <img src={Tv} alt="activity" className='w-full p-2 h-[300px]'/>
-              <h2>Dr. John Smith</h2>
-              <div>
-                <p className='text-start justify-start'>At the helm of ESTG is our esteemed principal, Dr. John Smith, a visionary leader with a deep passion for education and student development.....</p> 
+              
+              <div className='p-2'>
+              <h1 className='p-4'>Dr. John Smith 2</h1>
+                <p className='text-start justify-start'>At the helm of ESTG is our esteemed principal, Dr. John Smith, a visionary leader with a deep passion for education and student development.</p> 
                  <div className="p-1">
                    
 
@@ -76,25 +78,28 @@ const Campus = () => {
 
                     {/* Content that appears when checkbox is checked */}
                     <div className="hidden peer-checked:block">
-                      <p className='text-start mb-4'> With over 20 years of experience in the field, Dr. Smith has dedicated his career to fostering environments where students can excel academically, socially, and emotionally.</p>
-                      <label htmlFor="toggle2" className="cursor-pointer mt-5 ml-28 flex w-28 bg-blue-500 text-white p-2 rounded">
-                        See Less
-                      </label>
+                      <p className='text-start'> With over 20 years of experience in the field, Dr. Smith has dedicated his career to fostering environments where students can excel academically, socially, and emotionally.
+                        <label htmlFor="toggle2" className="cursor-pointer w-28 pl-2 text-blue-500 rounded">
+                          See Less
+                        </label>
+                      </p>
                     </div>
 
                      {/* Label acts as a clickable button */}
-                    <label htmlFor="toggle2" className="cursor-pointer mt-5 ml-28 flex w-28 peer-checked:hidden bg-blue-500 text-white p-2 rounded">
+                    <label htmlFor="toggle2" className="cursor-pointer w-28 peer-checked:hidden text-blue-500 p-2 rounded">
+                      <p className='absolute mt-[-28px] ml-[473px] cursor-default'>.....</p>
                       See More
                     </label>
                   </div>
               </div>
+              <img src={Running} alt="activity" className='w-[50%] p-2 h-[300px] rounded-md'/>
             </div>
-            <div className='w-full lg:w-[30%] shadow-lg border-opacity-5 rounded-md p-2 border-2 border-gray'>
+            <div className='w-full flex gap-5 shadow-lg mt-4 border-opacity-5 rounded-md p-2 border-gray'>
 
-              <img src={Refe} alt="activity" className='w-full p-2 h-[300px]'/>
-              <h2>Dr. John Smith</h2>
-              <div>
-                <p className='text-start justify-start'>At the helm of ESTG is our esteemed principal, Dr. John Smith, a visionary leader with a deep passion for education and student development.....</p> 
+              <img src={Running} alt="activity" className='w-[50%] p-2 h-[300px] rounded-md'/>
+              <div className='p-2'>
+                <h2 className='p-4'>Dr. John Smith 3</h2>
+                <p className='text-start justify-start'>At the helm of ESTG is our esteemed principal, Dr. John Smith, a visionary leader with a deep passion for education and student development.</p> 
                  <div className="p-1">
                    
 
@@ -103,14 +108,17 @@ const Campus = () => {
 
                     {/* Content that appears when checkbox is checked */}
                     <div className="hidden peer-checked:block">
-                      <p className='text-start mb-4'> With over 20 years of experience in the field, Dr. Smith has dedicated his career to fostering environments where students can excel academically, socially, and emotionally.</p>
-                      <label htmlFor="toggle3" className="cursor-pointer mt-5 ml-28 flex w-28 bg-blue-500 text-white p-2 rounded">
+                      <p className='text-start'> With over 20 years of experience in the field, Dr. Smith has dedicated his career to fostering environments where students can excel academically, socially, and emotionally.
+                      <label htmlFor="toggle3" className="cursor-pointer w-28 pl-2 text-blue-500 rounded">
                         See Less
                       </label>
+                      </p>
                     </div>
 
                      {/* Label acts as a clickable button */}
-                    <label htmlFor="toggle3" className="cursor-pointer mt-5 ml-28 flex w-28 peer-checked:hidden bg-blue-500 text-white p-2 rounded">
+        
+                    <label htmlFor="toggle3" className="cursor-pointer w-28 peer-checked:hidden text-blue-500 p-2 rounded">
+                      <p className='absolute mt-[-28px] ml-[473px] cursor-default'>.....</p>
                       See More
                     </label>
                   </div>
