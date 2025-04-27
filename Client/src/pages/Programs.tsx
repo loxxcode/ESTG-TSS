f4d7285906dd44624cc2b988f63887e9bcafdb50
loxxcode <loxxhypolite@gmail.com>
2025-04-27 18:52:41 +0200

    program updates

--- Client/src/pages/Programs.tsx
+++ Client/src/pages/Programs.tsx
@@ -19,16 +19,8 @@ const Programs = () => {
       <Navbar />
       
       {/* Page Header */}
-      <div className="pt-32 pb-16 md:pt-40 md:pb-20 bg-estg-gray-light dark:bg-black">
-        <div className="lg:ml-[250px] container px-4">
-          <AnimatedSection animation="slide-up" className="max-w-3xl">
-            <h1 className="text-4xl font-bold mb-4 text-black dark:text-white">
-              Academic Programs
-            </h1>
-            <p className="text-lg md:text-xl text-muted-foreground">
-              Discover our comprehensive range of undergraduate and graduate programs designed to prepare you for success in your chosen field.
-            </p>
-          </AnimatedSection>
+      <div className="pt-20 md:pb-4 bg-estg-gray-light dark:bg-black">
+        <div className="text-center container px-4">
           <AnimatedSection>
             <h2 className="text-4xl font-bold mb-2 text-black dark:text-white">Academic Programs</h2>
             <p className="text-black max-w-xl mx-auto mb-12 dark:text-white">
@@ -39,16 +31,16 @@ const Programs = () => {
       </div>
       
       {/* Programs Content - Placeholder, will be replaced with actual content */}
-      <div className="py-16 md:py-24 bg-white dark:bg-black">
-        <div className="container px-4">
+      <div className="bg-white lg:ml-[-150px] dark:bg-black">
+        <div className="container flex mb-32 px-4">
           <AnimatedSection animation="fade-in" className="text-center">
-            <div className='lg:ml-[200px] flex flex-col md:flex-row gap-10' >
-              <div className='flex flex-col gap-10'>
-              <img src={Elc} alt="Images" className='w-70% md:w-[90%] h-[300px]'/>
+            <div className='lg:ml-[200px] flex items-center justify-between md:flex-row flex-col gap-4' >
+              <div className='flex flex-col gap-5'>
+              <img src={Elc} alt="Electrical Technology" className='md:w-[500px] object-fit h-[300px]'/>
               <p className='text-black dark:text-white'>Electrical Technology</p>
               </div>
-              <div className='flex flex-col gap-10'>
-              <img src={Sod} alt="Images" className='w-70% md:w-[90%] h-[300px]'/>
+              <div className='flex flex-col gap-5'>
+              <img src={Sod} alt="Software Development" className='md:w-[500px] h-[300px]'/>
               <p className='text-black dark:text-white'>Software Development</p>
               </div>
             </div>
