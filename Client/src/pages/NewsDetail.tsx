ae6532896cb593dbb452080529f2c705ef671ee5
loxxcode <loxxhypolite@gmail.com>
2025-05-10 19:45:11 +0200

    changes on news details

--- Client/src/pages/NewsDetail.tsx
+++ Client/src/pages/NewsDetail.tsx
@@ -18,6 +18,8 @@ export default function NewsDetailPage() {
   const { id } = useParams();
   console.log("News id from Url",id);
   const navigate = useNavigate();
+  const [otherNews, setOtherNews] = useState<NewsItem[]>([]);
+  const [visibleNewsCount, setVisibleNewsCount] = useState(3);
 
   const [data, setData] = useState<NewsItem | null>(null);
   const [loading, setLoading] = useState(true);
@@ -40,6 +42,25 @@ export default function NewsDetailPage() {
     fetchData();
   }, [id]);
 
+  useEffect(() => {
+    const fetchAllNews = async () => {
+      try {
+        const response = await axios.get("http://localhost:5000/api/all_events");
+        const filtered = response.data.data.filter((item: NewsItem) => item.id !== Number(id));
+        setOtherNews(filtered);
+      } catch (error) {
+        console.error("Error fetching all news:", error);
+      }
+    };
+  
+    fetchAllNews();
+  }, [id]);
+  
+  const handleLoadMore = () => {
+    setVisibleNewsCount((prev) => prev + 3);
+  };
+  
+
   if (loading) return <div className="p-8 text-center">Loading...</div>;
   if (!data) return <NotFound />
 
@@ -56,6 +77,38 @@ export default function NewsDetailPage() {
         })}</p>
         <p className="text-gray-700">{data.description}</p>
        
+      </div>
+      <div className="max-w-6xl mx-auto mt-20 mb-10 px-10 ml-20">
+        <h2 className="text-2xl font-semibold mb-6">More News</h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {otherNews.slice(0, visibleNewsCount).map((item) => (
+            <div
+              key={item.id}
+              onClick={() => navigate(`/news/${item._id}`)}
+              className="cursor-pointer rounded-lg shadow hover:shadow-lg transition overflow-hidden"
+            >
+              <img src={item.imageUrl || Future} alt={item.title} className="h-48 w-full object-cover" />
+              <div className="p-4">
+                <p className="text-sm text-gray-800">
+                  {new Date(item.createdAt).toLocaleDateString()}
+                </p>
+                <h3 className="text-lg font-bold mt-1 text-gray-400">{item.title}</h3>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        {/* See More Button */}
+        {visibleNewsCount < otherNews.length && (
+          <div className="text-center mt-8">
+            <button
+              onClick={handleLoadMore}
+              className="px-6 py-2 bg-indigo-600 text-white rounded-full hover:bg-indigo-700 transition"
+            >
+              See More →
+            </button>
+          </div>
+        )}
       </div>
       <Footer />
     </section>
