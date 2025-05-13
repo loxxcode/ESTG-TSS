cb8639bcaead1a1d95e9ef6f860db7c23b1f51a4
benimanaobed12 <benkingcode@gmail.com>
2025-05-13 22:34:05 +0200

    forms of update and event creation

--- Client/src/pages/Adminpanel/Adminpages/update.tsx
+++ Client/src/pages/Adminpanel/Adminpages/update.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import Card from './Updatescards/cards';
+import Link from 'react-router-dom';
 
 function Event() {
   return (
     <div className="p-6 bg-estg-gray-light dark:bg-black min-h-screen ">
       <div className="flex  gap-4">
-      <a href="">
+      <a href="/createupdate">
+        
        <button
           className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-sm font-medium"
+
         >
-          Add Updates
+           Add Updates
+         
         </button>
+        
         </a>
       </div>
       <h1 className="text-2xl font-bold text-white-800 mb-5 mt-5">Updates Cards</h1>
