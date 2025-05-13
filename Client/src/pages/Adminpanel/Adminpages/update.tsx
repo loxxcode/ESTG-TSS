8a4c148d233d1ba3e1bb6ab985f1a1646f9659e1
ngabodaniel1000 <ngabodaniel1000@gmail.com>
2025-05-13 22:59:34 +0200

    resolving conflict in updates

--- Client/src/pages/Adminpanel/Adminpages/update.tsx
+++ Client/src/pages/Adminpanel/Adminpages/update.tsx
@@ -1,20 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import Card from './Updatescards/cards';
-<<<<<<< HEAD
 import axios from 'axios';
-=======
-import Link from 'react-router-dom';
->>>>>>> 18aec30034aae2190472fe95a2aa92ca7caccf70
+import { Link } from 'react-router-dom';
 
 function Event() {
   const [data, setData] = useState([]);
 
   const fetchData = async () => {
     try {
-      const response = await axios.get("http://localhost:5000/api/updates",{withCredentials:true});
-      
+      const response = await axios.get("http://localhost:5000/api/updates", { withCredentials: true });
       setData(response.data.data);
-
     } catch (error) {
       console.log(error);
     }
@@ -25,29 +20,15 @@ function Event() {
   }, []);
 
   return (
-<<<<<<< HEAD
     <div className="p-6 bg-estg-gray-light dark:bg-black min-h-screen">
       <div className="flex gap-4">
-        <a href="">
+        <Link to="/createupdate">
           <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-sm font-medium">
             Add Updates
           </button>
-=======
-    <div className="p-6 bg-estg-gray-light dark:bg-black min-h-screen ">
-      <div className="flex  gap-4">
-      <a href="/createupdate">
-        
-       <button
-          className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-sm font-medium"
-
-        >
-           Add Updates
-         
-        </button>
-        
->>>>>>> 18aec30034aae2190472fe95a2aa92ca7caccf70
-        </a>
+        </Link>
       </div>
+      
       <h1 className="text-2xl font-bold text-white-800 mb-5 mt-5">Updates Cards</h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
@@ -55,7 +36,7 @@ function Event() {
           <Card 
             key={index}
             title={item.title}
-            author={item.author.username}
+            author={item.author?.username}  // Added optional chaining in case author is null
             description={item.description}
             updatestype={item.type}
             onUpdate={() => console.log('Update', index)}
