1bddc75401ad812ddba9201ac1ec92a46f62f235
ngabodaniel1000 <ngabodaniel1000@gmail.com>
2025-05-13 23:26:51 +0200

    hiding navbar

--- Client/src/pages/Adminpanel/index.tsx
+++ Client/src/pages/Adminpanel/index.tsx
@@ -3,18 +3,18 @@ import Navbar from '../../components/layout/Navbar';
 import Update from './Adminpages/update';
 import Event from './Adminpages/event';
 import UserManagement from './Adminpages/usermagement';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 function Adminpanel() {
   const [activeTab, setActiveTab] = React.useState(0);
   const navigate = useNavigate()
-    React.useEffect(() => {
-  if (!localStorage.getItem("username")) {
-    navigate('/');
-  }
-}, [navigate]);
- const handleLogout = async () => {
+  React.useEffect(() => {
+    if (!localStorage.getItem("username")) {
+      navigate('/');
+    }
+  }, [navigate]);
+  const handleLogout = async () => {
     try {
       await axios.get('http://localhost:5000/api/account/logout', { withCredentials: true });
       navigate('/');
@@ -34,22 +34,31 @@ function Adminpanel() {
   return (
     <div className="min-h-screen bg-estg-gray-light dark:bg-black">
       {/* Fixed Navbar */}
-      <div className="fixed top-0 left-0 right-0 z-50 bg-estg-gray-light dark:bg-black">
+      {/* <div className="fixed top-0 left-0 right-0 z-50 bg-estg-gray-light dark:bg-black">
         <Navbar />
-      </div>
+      </div> */}
 
       {/* Layout after navbar */}
-      <div className="pt-16 flex">
+      <div className="pt-4 flex">
         {/* Fixed Sidebar */}
-        <aside className="hidden md:block fixed top-16 left-0 w-64 h-[calc(100vh-4rem)] bg-estg-gray-light dark:bg-black border-r shadow-md z-40">
+        <aside className="hidden md:block fixed top-16 left-0 w-64 h-[calc(94vh-4rem)] bg-estg-gray-light dark:bg-black border-r shadow-md z-40">
+          <div className='mt-[-40px] ml-10'>
+            <Link
+              to="/"
+              className="text-xl font-display font-bold tracking-tight text-estg-blue"
+            >
+              ESTG<span className="text-estg-blue">.</span>
+            </Link>
+          </div>
           <nav className="flex flex-col gap-2 p-4 bg-estg-gray-light dark:bg-black">
+
             {tabs.map((tab, index) => (
               <button
                 key={index}
                 onClick={() => setActiveTab(index)}
                 className={`px-4 py-2 rounded-md text-left transition-colors w-full ${activeTab === index
-                    ? 'bg-blue-600 text-white'
-                    : 'hover:bg-blue-600 text-white-700'
+                  ? 'bg-blue-600 text-white'
+                  : 'hover:bg-blue-600 text-white-700'
                   }`}
               >
                 <p className='text-dark-800 dark:text-white'>{tab.name}</p>
@@ -62,7 +71,7 @@ function Adminpanel() {
         {/* Main content with margin to avoid overlapping fixed sidebar */}
         <main className="flex-1 ml-0 md:ml-64 p-6 min-h-[calc(100vh-4rem)] bg-estg-gray-light dark:bg-black overflow-y-auto">
           {tabs[activeTab].component}
-          
+
         </main>
       </div>
     </div>
