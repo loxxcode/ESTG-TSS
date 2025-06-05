107caf2e9a3289fe5f2ba3c97f65dee7bca2b71a
ngabodaniel1000 <ngabodaniel1000@gmail.com>
2025-06-05 19:07:17 +0200

    ignoring forget password for user

--- Client/src/pages/Auth/User.tsx
+++ Client/src/pages/Auth/User.tsx
@@ -166,14 +166,14 @@ function User() {
                   </label>
                 </div>
 
-                <div className="text-sm">
+                {/* <div className="text-sm">
                   <Link
                     to="/forgot-password"
                     className="font-medium text-blue-600 hover:text-blue-500 dark:text-blue-400 dark:hover:text-blue-300"
                   >
                     Forgot password?
                   </Link>
-                </div>
+                </div> */}
               </div>
             </div>
 
